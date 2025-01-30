e269bf90eeffc2875c1ecd8058d427cf7e3f2a80
Saviour-Dagadu <savicane@gmail.com>
2025-01-30 15:32:37 +0000

    Adding Chamges made to index.js and vercel.json

--- .history/index_20250130150406.js
+++ .history/index_20250130150406.js
@@ -5,14 +5,19 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware to handle CORS
 app.use(cors());
 
+// Your API endpoint
 app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello from the API!',
-  });
+    res.json({
+        email: "savicane@gmail.com",
+        current_datetime: new Date().toISOString(),
+        github_url: "https://github.com/Saviour-Dagadu/Hng12-Api.git"
+    });
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`API is running on http://localhost:${port}`);
 });
